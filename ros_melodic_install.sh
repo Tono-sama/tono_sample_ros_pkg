57868965973a374e3c73d89d43893e4f26a5cc66
Tono-sama <tonoike121212@gmail.com>
2020-11-03 13:05:59 +0900

    [MODIF] apt-getをaptに変更, installに-yオプションを追加

--- ros_melodic_install.sh
+++ ros_melodic_install.sh
@@ -1,17 +1,17 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo apt -y update
+sudo apt -y upgrade
 
-sudo apt install ros-melodic-desktop-full
+sudo apt -y install ros-melodic-desktop-full
 apt search ros-melodic
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 source /opt/ros/melodic/setup.bash
 
-sudo apt install python-rosdep
+sudo apt -y install python-rosdep
 
 sudo rosdep init
 rosdep update
